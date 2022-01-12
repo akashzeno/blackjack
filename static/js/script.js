4f88a3784f5193b67ce642ad90e1ead2183cd5f0
Akash Zeno <77930996+akashzeno@users.noreply.github.com>
2022-01-12 19:00:22 +0530

    added comments (wip)

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,30 @@
 
+/*
+Generate a random number between 0 and the length of the cardsList array.
+
+Parameters
+----------
+cardsList : list
+    A list of strings representing the cards.
+
+Returns
+-------
+int
+    A random number between 0 and the length of the cardsList array.
+
+Examples
+--------
+>>> randomNumber()
+4
+*/
+/*
+Here's what the selected code is doing:
+1.The function randomNumber() is called.
+2. The function randomNumber() generates a random number between 0 and the length of the cardsList array.
+3. The function randomNumber() checks if the new random number is the same as the old random number.
+4. If the new random number is the same as the old random number, the function randomNumber() is called again.
+5. If the new random number is not the same as the old random number, newRandomNumber is returned.
+*/
 function randomNumber() {
     let newRandomNumber = Math.floor(Math.random() * cardsList.length);
 	if (newRandomNumber === oldRandomNumber) {
@@ -9,6 +35,50 @@ function randomNumber() {
 	}
 }
 
+/*
+Generates a random card from the cardsList and adds it to the player's hand.
+
+Parameters
+----------
+player : str
+    The player who is receiving the card.
+
+Returns
+-------
+img : HTMLImageElement
+    The card that was drawn.
+*/
+/*
+Here's what the selected code is doing:
+1.Generate a random number between 0 and 51.
+2. Get the card image from the cardsList array.
+3. Get the card number from the card image.
+4. If the card number is 11 and the player's total + 11 is greater than 21, change the card number to 1.
+5. Add the card number to the player's total.
+6. Create an image element and set its src to the card image.
+7. Append the image element to the player's cards element.
+8. If the player is the dealer, add the card number to the dealer's total.
+
+# **Step 7: Create the Dealer's Turn Function**
+#
+# The dealer's turn function is similar to the player's turn function. The only difference is that the dealer has to hit if their total is less than 17.
+#
+# Here's the code:
+#
+# ```javascript
+# function dealerTurn() {
+# if (dealer < 17) {
+# let img = generateCards("dealer");
+# dealerCards.appendChild(img);
+# } else {
+# dealerStand();
+# }
+# }
+# ```
+#
+# Here's what the code is doing:
+# 1. If the dealer
+*/
 function generateCards(player) {
 	let randNum = randomNumber();
 	let img = document.createElement("img");
@@ -34,6 +104,47 @@ function generateCards(player) {
 	return img;
 }
 
+/*
+checkBust(player)
+
+    Checks if the player busts.
+
+    Parameters
+    ----------
+    player : str
+        The player to check.
+
+    Returns
+    -------
+    bool
+        True if the player busts, False otherwise.
+
+    Examples
+    --------
+    >>> checkBust("you")
+    False
+    >>> checkBust("dealer")
+    False
+    >>> checkBust("you")
+    True
+    >>> checkBust("dealer")
+    True
+*/
+/*
+Here's what the selected code is doing:
+1.The function checkBust() takes in a player argument.
+2. If the player is you, it checks if you have a score higher than 21.
+3. If you do, it sets the resultText to "You Bust! Dealer Wins!" and disables the buttons.
+4. It also increments the losses variable by 1 and calls the scoreTracker() function to update the losses score.
+5. It also plays the awwSfx sound.
+6. If the player is dealer, it checks if the dealer has a score higher than 21.
+7. If the dealer does, it sets the resultText to "Dealer Bust! You Win!" and disables the buttons.
+8. It also increments the win variable by 1 and calls the scoreTracker() function to update the win score.
+9. It also plays the cashSfx sound.
+10. If the player is neither you or the dealer, it returns false.
+
+The next function is the disableButtons() function.
+*/
 function checkBust(player) {
 	if (player == "you") {
 		if (you > twentyOne) {
@@ -58,6 +169,41 @@ function checkBust(player) {
 	return false;
 }
 
+
+/*
+checkWin()
+
+    Checks if the player or dealer has won the game.
+
+    Parameters
+    ----------
+    dealer : int
+        The dealer's current score.
+    you : int
+        The player's current score.
+
+    Returns
+    -------
+    bool
+        True if the player or dealer has won, False otherwise.
+*/
+/*
+Here's what the selected code is doing:
+1.The function checkWin() is called when the player clicks the "Deal" button.
+2. The function checks if the player's total is greater than the dealer's total.
+3. If the player's total is greater than the dealer's total, the player wins.
+4. If the player's total is less than the dealer's total, the dealer wins.
+5. If the player's total is equal to the dealer's total, it's a tie.
+6. If the player wins, the dealer's cards are hidden and the player's cards are revealed.
+7. If the dealer wins, the player's cards are hidden and the dealer's cards are revealed.
+8. If it's a tie, the cards are hidden and a message is displayed.
+9. The function returns true if the player or dealer wins.
+10. The function returns false if the game is still in progress.
+
+# Step 7: Create the Game
+#
+# Now that we have the logic for the game, let
+*/
 function checkWin() {
 	if (dealer > you && dealer <= twentyOne) {
 		document.querySelector(".resultText").innerHTML = "Dealer Wins!";
@@ -77,11 +223,52 @@ function checkWin() {
 	return false;
 }
 
+/*
+Disables the buttons for the player to hit or stand.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+
+    Examples
+    --------
+    >>> disableButtons()
+*/
+/*
+Here's what the selected code is doing:
+1.The function disableButtons() is called when the player's turn is over.
+2. The function disables the hit and stand buttons.
+*/
 function disableButtons() {
 	document.querySelector(".hitBtn").disabled = true;
 	document.querySelector(".standBtn").disabled = true;
 }
 
+/*
+dealerLogic()
+
+    This function is called when the dealer's turn begins.
+
+    It adds a card to the dealer's hand and updates the dealer's total.
+
+    It then checks if the dealer has won or bust.
+
+    If the dealer has not won or bust, it calls itself again.
+
+    Args:
+        None
+
+    Returns:
+        None
+*/
+/*
+Here's what the selected code is doing:
+1.The dealerLogic function is called after the playerLogic function.
+*/
 function dealerLogic() {
     setTimeout(() => {
 
@@ -98,9 +285,30 @@ function dealerLogic() {
 	dealerLogic();
 
     }, 500);
-
 }
 
+/*
+scoreTracker(scoreName, scoreValue)
+
+Update the score display on the page.
+
+Parameters
+----------
+scoreName : str
+    The name of the score to update.
+scoreValue : int
+    The new value to display.
+
+Returns
+-------
+None
+*/
+/*
+Here's what the selected code is doing:
+1.The function scoreTracker() takes two arguments: scoreName and scoreValue.
+2. The function first checks if the scoreName is "you" or "dealer".
+3. If the scoreName is "you" or "dealer", the function updates their corresponding scoreValue to the scoreValue argument.
+*/
 function scoreTracker(scoreName, scoreValue) {
 	document.querySelector(`.${scoreName}`).innerHTML = `${scoreValue}`;
 }
